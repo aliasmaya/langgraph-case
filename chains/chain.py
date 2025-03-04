fae9a2102004fb324baede94dca9c33611cdbff2
LEO WONG <ceclecw@hotmail.com>
2025-03-04 17:15:51 +0800

    Support tools

--- chains/chain.py
+++ chains/chain.py
@@ -1,7 +1,11 @@
 import os
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 load_dotenv()
 
-chatbot = ChatOpenAI(temperature=0, model=os.getenv('MODEL', ''))
+tool = TavilySearchResults(max_results=5)
+bot = ChatOpenAI(temperature=0, model=os.getenv('MODEL', ''))
+# chatbot = bot.bind_tools([tool])
+chatbot = bot
